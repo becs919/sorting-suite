1e8f1eadffd2caa6c1c79a39d4eae30461c74a48
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-02-01 08:18:31 -0700

    clean up bubble sort and write pseudo code explaining how it works

--- scripts/bubble.js
+++ scripts/bubble.js
@@ -1,3 +1,34 @@
+
+// var myArray = [2, 1, 3, 5, 4];
+
+function bubbleSort(array) {
+  // START WITH OUT OF ORDER ARRAY
+  // LOOP THROUGH LENGTH OF ARRAY STARTING WITH INDEX 1 = NEXT VALUE
+  for(var i = 1; i < array.length; i++) {
+  // ASSIGN INDEX VALUE TO K
+    var k = array[i];
+  // LOOP THROUGH INDEXS STARTING WITH 0 = CURRENT VALUE.
+  // COMPARING CURRENT VALUE TO NEXT VALUE
+    for(var j = i-1; j >= 0 && array[j] > k; j--){
+  // IF CURRENT IS MORE THAN NEXT, J MOVES TO THE RIGHT ONE SPOT
+       array[j+1] = array[j];
+     };
+  // IF NOT, LOOP TO THE NEXT INDEX BY INCREASING BY ONE
+    array[j+1] = k;
+  };
+  // IF SORTED, RETURN SORTED ARRAY
+    return array;
+    console.log(array);
+};
+
+bubbleSort([2, 1, 3, 5, 4]);
+
+
+export default bubbleSort
+
+
+
+
 // function bubble(array){
 //   do {
 //     var sorted = true;
@@ -12,45 +43,3 @@
 //   } while (!sorted);
 //   return array;
 // }
-
-// var myArray = [2, 1, 3, 5, 4];
-
-function bubbleSort(array) {
-  // START WITH OUT OF ORDER ARRAY
-
-  // LOOP THROUGH ARRAY
-  for(var i = 1; i < array.length; i++) {
-  // GET INDEX VALUES
-    var k = array[i];
-  // LOOP THROUGH INDEXS STARTING WITH 0, COMPARE TO CURRENT INDEX, IF MORE THAN CURRENT INDEX
-    for(var j = i-1; j >= 0 && array[j] > k; j--){
-  // THEN THE INDEX MOVE TO RIGHT ONE SPACE
-       array[j+1] = array[j];
-     };
-  // IF NOT, LOOP TO THE NEXT INDEX BY INCREASING BY ONE
-    array[j+1] = k;
-  };
-
-    return array;
-    console.log(array);
-};
-
-
-bubbleSort([2, 1, 3, 5, 4]);
-
-
-// ONLY COMAPRES AND MOVES 0 INDEX. NOT LOOPING THROUGH ALL OF ARRAY, PRACTICE
-// function bubbleSort() {
-//   var myArray = [2, 1, 3, 5, 4]
-//
-//   for(var i = 0; i < myArray.length; i++) {
-//     if (myArray[0] > myArray[1]) {
-//       myArray.splice(1,0,myArray.splice(0,1)[0]);
-//       return myArray;
-//     }
-//     return myArray
-//   }
-// }
-
-
-export default bubbleSort
