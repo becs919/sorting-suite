797340cec30370626260b448cedc7ffa3604af83
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-02-03 11:24:47 -0700

    add in pausdo code

--- scripts/insertion.js
+++ scripts/insertion.js
@@ -1,6 +1,5 @@
 const insertionSort = (array) => {
-  debugger
-// loop through array
+// loop through array by incrementing starting with index of 1
   for (var i = 1; i < array.length; i++) {
     // get value based on index position
     let current = array[i];
@@ -8,15 +7,15 @@ const insertionSort = (array) => {
     for (var j = i - 1; array[j] > current; j--) {
       // if current is greater than next, switch with next
       array[j + 1] = array[j];
-    };
-    // if not, current moves to nect position and next moves
+    }
+    // if not, current moves to neXt position and next moves
     // loop through until sorted
     array[j + 1] = current;
-  };
+  }
   // once sorted, return array
-  return array
+  return array;
 };
 
-insertionSort([4, 1, 5, 3, 2])
+insertionSort([4, 1, 5, 3, 2]);
 
-export default insertionSort
+export default insertionSort;
